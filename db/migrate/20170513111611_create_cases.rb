594d4e858fc89cb04a48021fbfe3fa5041c6d0d3
tekwill <tekwill@tekwills-iMac.local>
2017-05-27 18:27:27 +0300

    change migrate version

--- db/migrate/20170513111611_create_cases.rb
+++ db/migrate/20170513111611_create_cases.rb
@@ -1,4 +1,4 @@
-class CreateCases < ActiveRecord::Migration[5.1]
+class CreateCases < ActiveRecord::Migration[5.0]
   def change
     create_table :cases do |t|
       t.string :image_link
--- db/migrate/20170513115904_add_user_id_to_cases.rb
+++ db/migrate/20170513115904_add_user_id_to_cases.rb
@@ -1,4 +1,4 @@
-class AddUserIdToCases < ActiveRecord::Migration[5.1]
+class AddUserIdToCases < ActiveRecord::Migration[5.0]
   def change
     add_column :cases, :user_id, :integer
   end
